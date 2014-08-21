c86116120e1db13abf668f7d3d5944018167fef0
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-21 12:17:09 +0000

    Import of SHARYANTO/Perinci-CmdLine-Examples-0.01 from CPAN.
    gitpan-cpan-distribution: Perinci-CmdLine-Examples
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Perinci-CmdLine-Examples-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Perinci/CmdLine/Examples.pm
@@ -0,0 +1,57 @@
+package Perinci::CmdLine::Examples;
+
+our $DATE = '2014-08-21'; # DATE
+# ABSTRACT
+
+use 5.010001;
+
+1;
+# ABSTRACT: Examples for Perinci::CmdLine
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Perinci::CmdLine::Examples - Examples for Perinci::CmdLine
+
+=head1 VERSION
+
+This document describes version 0.01 of Perinci::CmdLine::Examples (from Perl distribution Perinci-CmdLine-Examples), released on 2014-08-21.
+
+=head1 DESCRIPTION
+
+This distribution includes C<examples/> which contains example scripts that use
+L<Perinci::CmdLine>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Perinci-CmdLine-Examples>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Perinci-CmdLine-Examples>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Perinci-CmdLine-Examples>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
